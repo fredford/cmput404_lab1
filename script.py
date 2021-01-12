c9eb6cd465778910f1361040b5265230b90f9b8c
fredford <fredford@ualberta.ca>
2021-01-11 20:04:06 -0700

    Update script.py

--- script.py
+++ script.py
@@ -4,6 +4,6 @@ print("Requests version: " + requests.__version__)
 print("Google status code: " + str(requests.get("http://www.google.com/").status_code))
 print("GitHub Python Script:\n")
 
-r = requests.get("https://raw.github.com/fredford/cmput404_lab1/master/script.py")
+s = requests.get("https://raw.github.com/fredford/cmput404_lab1/master/script.py")
 
-print(r.text)
+print(s.text)
